abc984814989174e10f7e3d965644db2e1727887
Sergey Gurzhiy <gurzshisergey@gmail.com>
2023-06-20 17:07:09 +0300

    Correction dropdown list-2

--- index.js
+++ index.js
@@ -1,88 +1,17 @@
 const buttonGetWeather = document.getElementById("controler-submit");
 const widgets = document.getElementById("container-widgets");
-
-const cityInput = document.getElementById("city-input"); 
+const cityInput = document.getElementById("city-input");
 const dataSelect = document.getElementById("date-input");
 const checkboxSelect = document.getElementById("checkbox-input");
-
 const selectCityList = document.getElementById("search-list-city");
 
-// !!!!!!!!!!!!!!!!!!!!
-cityInput.addEventListener("focus", selectedCity);
-selectCityList.addEventListener("change", getSelectListCity)
-
+// cityInput.addEventListener("focus", searchCity);
+cityInput.addEventListener("input", searchCity);
+selectCityList.addEventListener("click", getSelectListCity);
 dataSelect.addEventListener("change", selectDate);
 checkboxSelect.addEventListener("change", selectCheckbox);
-
 buttonGetWeather.addEventListener("click", handleSubmit);
 
-
-function getSelectListCity(event) {
-  // console.log("select city:", event.target.value);
-  cityInput.value = event.target.value;
-  selectCityList.classList.add("hidden");
-}
-
-function selectedCity(event) {
-  // console.log("focus");
-  selectCityList.classList.remove("hidden");
-  cityInput.value = event.target.value;
-  // console.log(cityInput.value );
-}
-
-function selectDate(event) {
-  dataSelect.value = event.target.value;
-  // console.log('event.target.value_date', event.target.value); //value: date!
-  // console.log(document.getElementById('date-input').value); // 2-й вар.
-  // !!!!!!!!!!!!!! does not output to console !!!!!!!
-}
-
-// Create minLimitDate.
-let minLimitDate = new Date().toISOString().split("T")[0];
-
-// Create maxLimitDate and interval of date!!!
-let maxLimitDate = new Date(minLimitDate);
-maxLimitDate.setDate(maxLimitDate.getDate() + 4);
-maxLimitDate = maxLimitDate.toISOString().split("T")[0];
-
-dataSelect.setAttribute("min", minLimitDate);
-dataSelect.setAttribute("max", maxLimitDate);
-
-//
-function selectCheckbox(event) {
-  checkboxSelect.value = event.target.value;
-  // console.log('event.target.value_checkbox', event.target.value); //value: date!
-  // console.log('checkboxInput.checked', checkboxInput.checked);
-}
-
-function handleSubmit(event) {
-  event.preventDefault();
-  // console.log('button-get-weather: clicked');
-// console.log('cityInput.value:', cityInput.value);
-  // create object.
-  createObject = {
-    id: new Date().getTime().toString(),
-    city: cityInput.value,
-    dateDay: dataSelect.value,
-    isSave: checkboxSelect.checked, // on => true
-  };
-
-  // console.log("createObject", createObject);
-
-  // Перемещение обьекта createObject в массив widgetsDataArray.
-  // (с оригинальными значениями ключей.)
-  widgetsDataArray.push(createObject);
-  // console.log('widgetsDataArray', widgetsDataArray);
-   cityInput.value = '';
-  render();
-}
-
-
-// Create obgect.
-let createObject;
-
-// Create array.
-const widgetsDataArray = [];
 
 // Сreate List city select.
 const arrayCity = [
@@ -103,15 +32,108 @@ const arrayCity = [
   },
 ];
 
-// !!!
-for (let i = 0; i < arrayCity.length; i++) {
-  let option = document.createElement("option");
-  option.value = arrayCity[i].city;
-  option.textContent = arrayCity[i].city + "," + arrayCity[i].country;
-  // console.log(option);
-  selectCityList.appendChild(option);
-}
+// Create array
+let filterArrayCity = [];
 
+// Create obgect.
+let createObject;
+
+ let option = document.createElement("li");
+
+// Create array.
+const widgetsDataArray = [];
+
+
+function searchCity(event) {
+  // search city by word key
+  if (cityInput.value != "") {
+    cityInput.value = event.target.value;
+    console.log("search city by word key:", cityInput.value);
+
+    // filter list city
+    filterArrayCity = arrayCity.filter(   // 1 add include()
+      (el) => el.city.toLowerCase() === cityInput.value.toLowerCase()  // 2 condition for country  - some & every methods
+    );
+    console.log("filterArrayCity in searchCity;", filterArrayCity);
+
+
+    
+    //add item to list city
+    for (let i = 0; i < filterArrayCity.length; i++) {
+     
+      // option.value = arrayCity[i].city;
+      option.setAttribute("value", filterArrayCity[i].city);
+      option.textContent =
+        filterArrayCity[i].city + "," + filterArrayCity[i].country;
+      console.log("option:", option);
+      // option.addEventListener("click", getSelectListCity);
+      selectCityList.appendChild(option); // змiнна.
+      
+      filterArrayCity = [];
+    }
+
+    // show list city
+    selectCityList.classList.remove("hidden");
+  }
+};
+
+function getSelectListCity(event) {
+  // console.log("select city:", event.target.value);
+  const selectedValue = event.target.getAttribute("value");
+  console.log("select", selectedValue); // ??????
+  cityInput.value = selectedValue;
+  selectCityList.classList.add("hidden");
+  
+  filterArrayCity = [];
+  console.log("getSelectListCity   filterArrayCity",  filterArrayCity);
+  // clear selectCityList
+    //  selectCityList.appendChild(option);
+  selectCityList.removeChild(option);
+};
+
+
+// Create minLimitDate.
+let minLimitDate = new Date().toISOString().split("T")[0];
+// Create maxLimitDate and interval of date!!!
+let maxLimitDate = new Date(minLimitDate);
+maxLimitDate.setDate(maxLimitDate.getDate() + 4);
+maxLimitDate = maxLimitDate.toISOString().split("T")[0];
+
+dataSelect.setAttribute("min", minLimitDate);
+dataSelect.setAttribute("max", maxLimitDate);
+
+
+function selectDate(event) {
+  dataSelect.value = event.target.value;
+};
+
+
+function selectCheckbox(event) {
+  checkboxSelect.value = event.target.value;
+};
+
+function handleSubmit(event) {
+  event.preventDefault();
+
+  createObject = {
+    id: new Date().getTime().toString(),
+    city: cityInput.value,
+    dateDay: dataSelect.value,
+    isSave: checkboxSelect.checked, // on => true
+  };
+
+  // Перемещение обьекта createObject в массив widgetsDataArray.
+  // (с оригинальными значениями ключей.)
+  widgetsDataArray.push(createObject);
+  console.log("createObject", createObject);
+
+  // clear form
+  cityInput.value = "";
+  dataSelect.value = minLimitDate;
+  checkboxSelect.checked = false;
+
+  render();
+};
 
 function render() {
   const renderHTML = ` 
@@ -148,4 +170,4 @@ function render() {
 </div>
 `;
   widgets.innerHTML += renderHTML;
-}
+};
